70a552f93efe05e4bc130b6b79f4a2cbcdee13c6
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2024-09-26 18:45:09 +0530

    This is a my new code

--- ArrayListPW/ArrayReversal.java
+++ ArrayListPW/ArrayReversal.java
@@ -1,80 +1,95 @@
 package ArrayListPW;
 
-import java.util.*;
-
+import java.util.HashMap;
 
 public class ArrayReversal {
 
-      static HashMap<Character,Integer> makefreqMap = new HashMap<>(){
-            HashMap <Character,Integer> mp = new HashMap<>();
-            for(int i=0;i<str.length();i++){
-                  Character ch = str.charAt(i);
-                  if(!mp.containsKey(ch)){
-                        mp.put(ch,1);
-                  }else{
-                        int currFreq = mp.get(ch);
-                        mp.put(ch, currFreq+1);
-                  }
+    // Method to create a frequency map of characters from a string
+    public static HashMap<Character, Integer> makefreqMap(String str) {
+        HashMap<Character, Integer> mp = new HashMap<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (!mp.containsKey(ch)) {
+                mp.put(ch, 1);
+            } else {
+                int currFreq = mp.get(ch);
+                mp.put(ch, currFreq + 1);
             }
-            return np;
-      }
-
-
-
-
-      public static boolean isAnagram(String s, String t){
-            if(s.length() != t.length())return false;
-            HashMap<Integer,Character> map1 = new HashMap<>();
-            HashMap<Integer,Character> map2 = new HashMap<>();
-            return map1.equals(map2);
-      }
-
-
-      public boolean canArrange(int[] arr, int k) {
-        HashMap<Integer,Integer> map = new HashMap<>();
-        for(int i=0;i<arr.length;i++){
-            int ele = arr[i]%k;
-            if(map.containsKey(ele)){
-                  int freq = map.get(ele);
-                  map.put(ele,freq+1);
-            }
-            else map.put(ele,1);
         }
-        for( int key: map.keySet()){
-            int rem = k-key;
-            if(!map.containsKey(rem)) return false;
-            int keyFreq =map.get(key);
-            int remFreq =map.get(rem);
-            if(keyFreq !=remFreq)return false;
+        return mp;
+    }
+
+    // Method to check if two strings are anagrams
+    public static boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) return false;
+
+        HashMap<Character, Integer> map1 = makefreqMap(s);
+        HashMap<Character, Integer> map2 = makefreqMap(t);
+
+        return map1.equals(map2);  // Compare both frequency maps
+    }
+
+    // Method to check if an array can be arranged in pairs divisible by k
+    public boolean canArrange(int[] arr, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        
+        for (int i = 0; i < arr.length; i++) {
+            int ele = ((arr[i] % k) + k) % k;  // Handle negative remainders
+            map.put(ele, map.getOrDefault(ele, 0) + 1);
+        }
+
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+
+            if (key == 0) {  // Special case for remainder 0
+                if (freq % 2 != 0) return false;
+            } else {
+                int rem = (k - key) % k;  // Complement remainder
+                if (!map.containsKey(rem) || map.get(rem) != freq) {
+                    return false;
+                }
+            }
         }
         return true;
-      }
+    }
 
-      public static void main(String[] args) {
-      //       int[] arr = { 10, 20, 30, 40, 50, 60, 70 };
-      //       int n = arr.length;
-
-      //       // Print original array
-      //       for (int ele : arr) {
-      //             System.out.print(ele + " ");
-      //       }
-      //       System.out.println();
-
-      //       // Reverse the array
-      //       int i = 0, j = n - 1;
-      //       while (i <= j) {
-      //             int temp = arr[i];
-      //             arr[i] = arr[j];
-      //             arr[j] = temp;
-      //             i++;
-      //             j--;  // Decrement j
-      //       }
-
-      //       // Print reversed array
-      //       for (int ele : arr) {
-      //             System.out.print(ele + " ");
-      //       }
-      //       System.out.println();
-      // }
+    public int countNicePairs(int[] nums) {
+ int count = 0;
+ int n= nums.length;
+ HashMap<Integer, Integer>map = new HashMap<>();
+ for(int i=0;i<n-1;i++){
+int ele = nums[i]- rev(nums[i]);
+if(map.containsKey(ele)){
+    int freq =map.get(ele);
+    count += freq;
+    map.put(ele,freq+1);
 }
+else map.put(ele,1);
+ }
+        return count;
+        
+    }
+     
+    public static int rev(int n){
+        int r=0;
+        while (n!=0) {
+            r= r*10 +n%10;
+            n /= 10;
+        }
+        return r;
+    }
+
+
+    public static void main(String[] args) {
+        // Example usage of isAnagram method
+        String s1 = "listen";
+        String s2 = "silent";
+        System.out.println(isAnagram(s1, s2));  // Output: true
+
+        // Example usage of canArrange method
+        ArrayReversal ar = new ArrayReversal();
+        int[] arr = {1, 2, 3, 4, 5, 10, 6, 7, 8, 9};
+        int k = 5;
+        System.out.println(ar.canArrange(arr, k));  // Output: true
+    }
 }
