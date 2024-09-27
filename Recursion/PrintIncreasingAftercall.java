ed2a274a667a578edb39c038e8756a6e20ea1c72
1611navneetkumar@gmail.com <1611navneetkumar@gmail.com>
2024-09-27 19:03:29 +0530

    Doing a new concept of recursion

---
+++ Recursion/PrintIncreasingAftercall.java
@@ -0,0 +1,21 @@
+package Recursion;
+
+import java.util.Scanner;
+
+public class PrintIncreasingAftercall {
+
+      public static void print(int n) {
+            if (n == 0)
+                  return;
+
+            print(n - 1);
+            System.out.println(n);
+      }
+
+      public static void main(String[] args) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter the number");
+            int n = sc.nextInt();
+            print(n);
+      }
+}
